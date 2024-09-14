355b8f51c4bf8d19fdf620d59500fb6dd8153e03
annae <anna.romagosa@smartassets.ai>
2024-09-14 12:22:39 +0200

    login screen done

--- src/screens/login/index.tsx
+++ src/screens/login/index.tsx
@@ -15,10 +15,23 @@ const LoginScreen = () => {
             Hi, welcome back!
           </Text>
           <Text size="s2" textAlign="center" color="b500">
-            Hope you are doing fine.
+            Hope you are doing fine :)
           </Text>
         </View>
         <LoginForm />
+        <View>
+          <Text size="s2" color="blue">
+            Forgot password?
+          </Text>
+        </View>
+        <View style={styles.signUpContainer}>
+          <Text size="s2" color="b500">
+            Don't have an account yet?
+          </Text>
+          <Text size="s2" color="blue">
+            Sign up
+          </Text>
+        </View>
       </View>
     </PageWrapper>
   )
@@ -28,7 +41,7 @@ export default LoginScreen
 
 const styles = StyleSheet.create({
   pageContainer: {
-    marginTop: getStatusBarHeight() + theme.space.xl,
+    marginTop: getStatusBarHeight() + theme.space.xl2,
     width: '100%',
     alignItems: 'center',
     gap: theme.space.xl,
@@ -37,5 +50,10 @@ const styles = StyleSheet.create({
   titleContainer: {
     alignItems: 'center',
     gap: theme.space.sm
+  },
+  signUpContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: theme.space.xs2
   }
 })
